58842aefa1dc3aade0579518bb7e1f41bd2afb55
vldcndn <vladie.condeno1495@gmail.com>
2021-08-24 22:03:15 +0800

    first init

---
+++ database/migrations/2021_08_24_135418_create_songs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('artist_id');
+            $table->text('lyrics');
+            $table->date('release_date');
+            $table->integer('added_by_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
